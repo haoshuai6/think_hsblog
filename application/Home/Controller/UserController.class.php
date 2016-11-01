9bbae48496c43e2c87886697eeb039bdfaabb792
haoshuai_it <haoshuai_it@163.com>
2016-11-01 17:30:40 +0800

    update theme

---
+++ application/Home/Controller/UserController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haoshuai_it
+ * Date: 2016-11-01
+ * Time: 17:01
+ */
+namespace Home\Controller;
+use Common\Controller\HomeBaseController;
+class UserController extends HomeBaseController {
+    public function center(){
+        $this->display();
+    }
+}
